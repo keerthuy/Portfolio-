7da67f786122f0857bdcc37369dfcedc5fceacf3
keerthuy <keerththanan109@gmail.com>
2026-03-29 15:21:13 +0530

    add correct mail

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
           <Link href="https://www.linkedin.com/in/gopalakrishnar-keerththanan-a424a02a8/" target="_blank" className="p-3 rounded-full bg-white/5 hover:bg-white/10 text-slate-300 hover:text-white hover:scale-110 transition-all border border-white/10">
             <LinkedinIcon className="w-5 h-5" />
           </Link>
-          <Link href="mailto:hello@example.com" className="p-3 rounded-full bg-white/5 hover:bg-white/10 text-slate-300 hover:text-white hover:scale-110 transition-all border border-white/10">
+          <Link href="mailto:keerththanan109@gmail.com" className="p-3 rounded-full bg-white/5 hover:bg-white/10 text-slate-300 hover:text-white hover:scale-110 transition-all border border-white/10">
             <Mail className="w-5 h-5" />
           </Link>
         </div>
